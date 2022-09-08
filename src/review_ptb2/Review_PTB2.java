053e03c2c836f4d2bf9cbdfa87febe20031ae8e8
LV NT <levnhattan@gmail.com>
2022-09-08 10:58:56 +0700

    Commit lan1

---
+++ src/review_ptb2/Review_PTB2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package review_ptb2;
+
+/**
+ *
+ * @author Nhat Tan
+ */
+public class Review_PTB2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
